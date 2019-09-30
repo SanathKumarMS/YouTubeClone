defeea35df9464e7b2421a6fd771a9ade7705d07
Sanath Kumar M S <sanathkumar.ms@ymedialabs.com>
2019-09-30 14:50:48 +0530

    First Commit

---
+++ YouTubeClone/View Model/BaseVM.swift
@@ -0,0 +1,9 @@
+//
+//  BaseVM.swift
+//  YouTubeClone
+//
+//  Created by Sanath Kumar M S on 30/09/19.
+//  Copyright © 2019 Sanath Kumar M S. All rights reserved.
+//
+
+import Foundation
---
+++ YouTubeClone/View/HomeVC.swift
@@ -0,0 +1,13 @@
+//
+//  HomeVC.swift
+//  YouTubeClone
+//
+//  Created by Sanath Kumar M S on 30/09/19.
+//  Copyright © 2019 Sanath Kumar M S. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: BaseVC {
+
+}
